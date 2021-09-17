ea292ae770e2edd9bc89eb2b429f993cc9bdd660
ricksonre <ricksonre@gmail.com>
2021-09-16 22:58:19 -0700

    Added a comment
    - Added a comment to the dinf method

--- ElementFinder.py
+++ ElementFinder.py
@@ -1,4 +1,5 @@
 def find(number,elements):
+    # Find if a element is present in a array. Returns true if found, false otherwise.
     for element in elements:
         if element == number:
             return True
